388086766e8b83a8ac1af4a01335ffbcb31a0b58
abdelazizfreelancer-cyber <abdelaziz.freelancer@gmail.com>
2026-09-05 10:25:42 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // اللي متسجلش هنا خالص ومحفوظة كـ Environment Variable على Vercel بس.
 
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://obysvsjesvljbtnupedi.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ieXN2c2plc3ZsamJ0bnVwZWRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2NDAzNzMsImV4cCI6MjEwMzIxNjM3M30.ptA4ooLpkqZW0825CsQ8UL4ZZqPrhfjE5mhWlo8U7Gk",
+  SUPABASE_URL: "https://ikzejyosymihahfliesl.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlremVqeW9zeW1paGFoZmxpZXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4NTM0MTYsImV4cCI6MjEwMzQyOTQxNn0.VDqVUJJK2dKZVH8eHMX738eUlaTCUvng_V-3DVmpy7o",
   GOOGLE_SHEET_VIEW_URL: "https://docs.google.com/spreadsheets/d/12tIuEMwi5acSKR1EAbb5lNQCcfVkSa6AAZeYckVZc3Y/edit"
 };
